6a4fb05caea0c7384de073d96a0e432a67be34d9
senbeiman <1000bei10000@gmail.com>
2021-01-29 00:03:23 +0900

    add debug pring

--- backend/index.ts
+++ backend/index.ts
@@ -8,11 +8,23 @@ app.use(express.static('build-react'))
 const PORT = 3001;
 
 app.get('/keyboard_show', (_req, _res) => {
-  childProcess.exec('DISPLAY=:0 wmctrl -a keyboard')
+  childProcess.exec('DISPLAY=:0 wmctrl -a keyboard', (err, stdout, stderr) => {
+    if (err) {
+      console.log(`stderr: ${stderr}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+  })
 });
 
 app.get('/keyboard_hide', (_req, _res) => {
-  childProcess.exec('DISPLAY=:0 wmctrl -a chromium')
+  childProcess.exec('DISPLAY=:0 wmctrl -a chromium', (err, stdout, stderr) => {
+    if (err) {
+      console.log(`stderr: ${stderr}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+  })
 });
 
 app.listen(PORT, () => {
