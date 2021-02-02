45b95722d254475539053e52eb91b7d57985ac7f
senbeiman <1000bei10000@gmail.com>
2021-02-02 12:43:36 +0900

    add noto fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 import CreateTodo from './components/CreateTodo';
 import MainPage from './components/MainPage';
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Noto Sans',
+      'Noto Sans CJK JP',
+      'sans-serif'
+    ].join(',')
+  }
+})
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path='/create_periodic'>
-          <CreateTodo todoType={'periodic'}/>
-        </Route>
-        <Route path='/create_temporary'>
-          <CreateTodo todoType={'temporary'}/>
-        </Route>
-        <Route path='/'>
-          <MainPage />
-        </Route>
-      </Switch>
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path='/create_periodic'>
+            <CreateTodo todoType={'periodic'}/>
+          </Route>
+          <Route path='/create_temporary'>
+            <CreateTodo todoType={'temporary'}/>
+          </Route>
+          <Route path='/'>
+            <MainPage />
+          </Route>
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   )
 }
 
