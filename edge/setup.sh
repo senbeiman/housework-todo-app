7a58c2e353d094bb0537e9d4d4631db8f3d80d77
senbeiman <1000bei10000@gmail.com>
2021-02-07 13:52:23 +0900

    fix

--- edge/setup.sh
+++ edge/setup.sh
@@ -8,6 +8,7 @@ git clone https://github.com/senbeiman/housework-todo-app.git
 cd ./housework-todo-app/edge
 sudo \cp -f config.txt /boot/
 sudo \cp -f 40-libinput.conf /usr/share/X11/xorg.conf.d/
+cp run_housework_todo_app.sh /home/pi/Desktop/
 sudo cp housework.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable housework.service
